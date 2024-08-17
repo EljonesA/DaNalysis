d5dda0553b743c2ef904a85f8addfb4a5e326de6
Eljones <eljones.odongo@gmail.com>
2024-08-17 19:47:06 +0300

    Stripe payment, docstrings along other updates

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-# app.py
 from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 import pyrebase
 from config import firebaseConfig
 import pandas as pd
 import plotly.express as px
 import datetime
+import stripe
 
 app = Flask(__name__)
-app.secret_key = 'your-secret-key'
+app.secret_key = 'secrt7436-tehdhhe'
+stripe.api_key = 'sk_test_51Pn2ifK4FVOPIRC2iy1vtQfyHkbWI6S8CdXlWbMO1DPujPtlUgoSu3e4s9IvKyMDhsqGe3RAFuzWusUVZrliMv8d00HVfNHReK'
 
 # Initialize Firebase
 firebase = pyrebase.initialize_app(firebaseConfig)
@@ -15,10 +16,20 @@ auth = firebase.auth()
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    """
+    Handle user login functionality.
+
+    Renders the login page on GET request.
+    On POST request, attempts to authenticate the user using Firebase with the provided
+    email and password. If authentication is successful, the user is redirected to the 
+    landing page. If authentication fails, an error message is displayed.
+
+    Returns:
+        str: HTML template for the login page or a redirect to the landing page.
+    """
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-        print(f"{email}: {password}")
         try:
             user = auth.sign_in_with_email_and_password(email, password)
             session['user'] = user['idToken']
@@ -29,6 +40,17 @@ def login():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    """
+    Handle user signup functionality.
+
+    Renders the signup page on GET request.
+    On POST request, attempts to create a new user account in Firebase with the provided 
+    email and password. If the signup is successful, the user is redirected to the login page. 
+    If passwords do not match or signup fails, an error message is displayed.
+
+    Returns:
+        str: HTML template for the signup page or a redirect to the login page.
+    """
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
@@ -45,11 +67,25 @@ def signup():
 
 @app.route('/logout')
 def logout():
+    """
+    Log out the currently logged-in user.
+    Clears the user session and redirects to the login page.
+
+    Returns:
+        str: Redirect to the login page.
+    """
     session.pop('user', None)
     return redirect(url_for('login'))
 
 @app.route('/')
 def landing_page():
+    """
+    Render the landing page for authenticated users.
+    If the user is not logged in, they are redirected to the login page.
+
+    Returns:
+        str: HTML template for the landing page or a redirect to the login page.
+    """
     if 'user' not in session:
         return redirect(url_for('login'))
     return render_template('landing_Page.html')
@@ -58,6 +94,13 @@ def landing_page():
 ''' =====> Macroeconomic Analysis routes & logic <===== '''
 @app.route('/macroeconomic_analysis')
 def macroeconomic_analysis():
+    """
+    Render the Macroeconomic Analysis page.
+    If the user is not logged in, they are redirected to the login page.
+
+    Returns:
+        str: HTML template for the Macroeconomic Analysis page or a redirect to the login page.
+    """
     if 'user' not in session:
         return redirect(url_for('login'))
     return render_template('macroeconomic_analysis.html')
@@ -65,6 +108,16 @@ def macroeconomic_analysis():
 # filters
 @app.route('/fetch_filtered_data')
 def fetch_filtered_data():
+    """
+    Fetch and filter Nominal GDP, Historical Population, or GDP Per Capita data.
+
+    Based on the selected chart type ('gdp', 'population', 'per_capita') and period ('5Y', '10Y', 'Full'),
+    this function reads the corresponding dataset, filters it, generates a Plotly chart, and returns 
+    the chart as HTML.
+
+    Returns:
+        json: JSON object containing the HTML of the generated Plotly chart or an error message.
+    """
     if 'user' not in session:
         return redirect(url_for('login'))
 
@@ -106,6 +159,15 @@ def fetch_filtered_data():
 
 @app.route('/fetch_filtered_exchanges')
 def fetch_filtered_exchanges():
+    """
+    Fetch and filter exchange rate data.
+
+    Based on the selected period ('5Y', '2Y', 'YTD', '3M', '1M', 'Full'), this function reads the exchange 
+    rate dataset, filters it, generates a Plotly chart, and returns the chart as HTML.
+
+    Returns:
+        json: JSON object containing the HTML of the generated Plotly chart or an error message.
+    """
     if 'user' not in session:
         return redirect(url_for('login'))
 
@@ -156,6 +218,15 @@ def fetch_filtered_exchanges():
 
 @app.route('/fetch_filtered_inflation')
 def fetch_filtered_inflation():
+    """
+    Fetch and filter inflation rate data.
+
+    Based on the selected period ('3M', '1M', 'Full'), this function reads the inflation rate dataset, 
+    filters it, generates a Plotly chart, and returns the chart as HTML.
+
+    Returns:
+        json: JSON object containing the HTML of the generated Plotly chart or an error message.
+    """
     if 'user' not in session:
         return redirect(url_for('login'))
 
@@ -186,6 +257,15 @@ def fetch_filtered_inflation():
 
 @app.route('/fetch_filtered_shares')
 def fetch_filtered_shares():
+    """
+    Fetch and filter share prices data.
+
+    Based on the selected period ('2M', '5D', 'Full'), this function reads the share prices dataset, 
+    filters it, generates a Plotly chart, and returns the chart as HTML.
+
+    Returns:
+        json: JSON object containing the HTML of the generated Plotly chart or an error message.
+    """
     if 'user' not in session:
         return redirect(url_for('login'))
 
@@ -224,6 +304,15 @@ def fetch_filtered_shares():
 
 @app.route('/fetch_filtered_shares_more')
 def fetch_filtered_shares_more():
+    """
+    Fetch and filter share prices data.
+
+    Based on the selected period ('3M', '1M', '5D', 'Full'), this function reads the share prices dataset, 
+    filters it, generates a Plotly chart, and returns the chart as HTML.
+
+    Returns:
+        json: JSON object containing the HTML of the generated Plotly chart or an error message.
+    """
     if 'user' not in session:
         return redirect(url_for('login'))
 
@@ -265,6 +354,16 @@ def fetch_filtered_shares_more():
 
 @app.route('/dashboard1')
 def dashboard1():
+    """
+    Render the first dashboard showing Nominal GDP, Population Trend, and GDP Per Capita.
+
+    If the user is not logged in, they are redirected to the login page.
+    This route reads CSV data for each metric, generates Plotly visualizations, 
+    and renders them in the 'dashboard1.html' template.
+
+    Returns:
+        str: HTML template for the first dashboard with embedded Plotly graphs.
+    """
     if 'user' not in session:
         return redirect(url_for('login'))
 
@@ -304,9 +403,20 @@ def dashboard1():
 
 @app.route('/dashboard2')
 def dashboard2():
+    """
+    Render the second dashboard showing USD/KES exchange rates.
+
+    If the user is not logged in, they are redirected to the login page.
+    This route reads CSV data for exchange rates, generates a Plotly visualization, 
+    and renders it in the 'dashboard2.html' template.
+
+    Returns:
+        str: HTML template for the second dashboard with an embedded Plotly graph.
+    """
     if 'user' not in session:
         return redirect(url_for('login'))
-    # GDP Per Capita processing & plotting
+
+    # Exchange rates processing & plotting
     df = pd.read_csv('datasets/Exchange_Rates.csv')
     fig = px.line(df, x='Date', y='Mean')
     fig.update_layout(
@@ -319,12 +429,32 @@ def dashboard2():
 
 @app.route('/dashboard3')
 def dashboard3():
+    """
+    Render the third dashboard with premium content.
+
+    If the user is not logged in, they are redirected to the login page.
+    The template 'dashboard3.html' will contain the premium content, 
+    and access will be controlled by the user's subscription status.
+
+    Returns:
+        str: HTML template for the third dashboard.
+    """
     if 'user' not in session:
         return redirect(url_for('login'))
     return render_template('dashboard3.html')
 
 @app.route('/inflation_monthly')
 def inflation_monthly():
+    """
+    Render the page comparing current month's inflation vs. prior month's inflation.
+
+    If the user is not logged in, they are redirected to the login page.
+    This route reads CSV data for monthly inflation rates, generates a Plotly bar chart, 
+    and renders it in the 'inflation_comparison.html' template.
+
+    Returns:
+        str: HTML template with an embedded Plotly graph comparing monthly inflation.
+    """
     # freely accessible page
     if 'user' not in session:
         return redirect(url_for('login'))
@@ -345,6 +475,17 @@ def inflation_monthly():
 
 @app.route('/inflation_trend')
 def inflation_trend():
+    """
+    Render the page showing the historical inflation trend (premium content).
+
+    If the user is not logged in, they are redirected to the login page.
+    The 'is_premium' parameter determines whether the user has access to premium content.
+    This route reads CSV data for inflation rates, generates a Plotly line chart, 
+    and renders it in the 'inflation_premium.html' template.
+
+    Returns:
+        str: HTML template with an embedded Plotly graph showing the inflation trend.
+    """
     # premium content page
     if 'user' not in session:
         return redirect(url_for('login'))
@@ -369,40 +510,163 @@ def inflation_trend():
 
 @app.route('/paywall')
 def paywall():
+    """
+    Render the paywall page for premium content access.
+
+    This route serves the 'paywall.html' template, which prompts the user 
+    to select a payment method to gain access to premium content.
+
+    Returns:
+        str: HTML template for the paywall.
+    """
+    if 'user' not in session:
+        return redirect(url_for('login'))
     return render_template('paywall.html')
 
 @app.route('/process_payment', methods=['POST'])
 def process_payment():
-    full_name = request.form['full_name']
-    address = request.form['address']
-    card_number = request.form['card_number']
-    expiry_date = request.form['expiry_date']
-    cvv = request.form['cvv']
+    """
+    Handle payment processing for premium content access.
 
-    # Here you would integrate with a payment gateway to process the payment
-    # For example, using Stripe:
-    # stripe.Charge.create(
-    #     amount=amount_in_cents,
-    #     currency='usd',
-    #     source=card_token,
-    #     description='Premium Membership'
-    # )
+    Depending on the selected payment method, this route handles the payment via Stripe 
+    for Visa or integrates with M-Pesa SDK for mobile payments.
+    If payment is successful, the user is redirected to access premium content.
 
-    # Assuming payment is successful:
-    flash('Payment successful! You are now a premium member.', 'success')
-    return redirect(url_for('inflation_trend', is_premium=True))
+    Returns:
+        str: Redirect to the inflation trend page if payment is successful,
+             or to the paywall page if payment fails.
+    """
+    payment_method = request.form.get('payment')
+
+    if payment_method == 'visa':
+        # Get form data including stripe token
+        token = request.form['stripeToken']
+        customer_name = request.form['customer_name']
+        customer_address = request.form['customer_address']
+        price = request.form['total_price']
+
+        try:
+            if (int(price) == 0):
+                desc = "DaNalysis Premium Membership (Coupon applied) - Inflation"
+            else:
+                desc = "DaNalysis Premium Membership - Inflation"
+            # Create a charge using the token
+            charge = stripe.Charge.create(
+                amount=10000,  # Amount in cents
+                currency='kes',
+                description=desc,
+                source=token,
+                metadata={
+                    'customer_name': customer_name,
+                    'customer_address': customer_address
+                }
+            )
+            return redirect(url_for('inflation_trend', is_premium=True))
+        except stripe.error.StripeError as e:
+            return redirect(url_for('paywall'))
+    elif payment_method == 'mpesa':
+        print("MPESA method")
+        full_name = request.form['full_name']
+        mobile_number = request.form['mobile_number']
+        # Integrate with M-Pesa SDK here
+        flash('M-Pesa payment initiated! Follow the instructions on your phone.', 'success')
+        return redirect(url_for('inflation_trend', is_premium=True))
+
+@app.route('/paywall_shares')
+def paywall_shares():
+    """
+    Render the paywall page for premium content access.
+
+    This route serves the 'paywall.html' template, which prompts the user 
+    to select a payment method to gain access to premium content.
+
+    Returns:
+        str: HTML template for the paywall.
+    """
+    if 'user' not in session:
+        return redirect(url_for('login'))
+    return render_template('paywall_shares.html')
+
+@app.route('/process_payment_shares', methods=['POST'])
+def process_payment_shares():
+    """
+    Handle payment processing for premium content access.
+
+    Depending on the selected payment method, this route handles the payment via Stripe 
+    for Visa or integrates with M-Pesa SDK for mobile payments.
+    If payment is successful, the user is redirected to access premium content.
+
+    Returns:
+        str: Redirect to the inflation trend page if payment is successful,
+             or to the paywall page if payment fails.
+    """
+    payment_method = request.form.get('payment')
+
+    if payment_method == 'visa':
+        # Get form data including stripe token
+        token = request.form['stripeToken']
+        customer_name = request.form['customer_name']
+        customer_address = request.form['customer_address']
+        price = request.form['total_price']
+
+        try:
+            if (int(price) == 0):
+                desc = "DaNalysis Premium Membership (Coupon applied) - Shares"
+            else:
+                desc = "DaNalysis Premium Membership - Shares"
+            # Create a charge using the token
+            charge = stripe.Charge.create(
+                amount=10000,  # Amount in cents
+                currency='kes',
+                description=desc,
+                source=token,
+                metadata={
+                    'customer_name': customer_name,
+                    'customer_address': customer_address
+                }
+            )
+            return redirect(url_for('page2', is_premium=True))
+        except stripe.error.StripeError as e:
+            return redirect(url_for('paywall'))
+    elif payment_method == 'mpesa':
+        print("MPESA method")
+        full_name = request.form['full_name']
+        mobile_number = request.form['mobile_number']
+        # Integrate with M-Pesa SDK here
+        flash('M-Pesa payment initiated! Follow the instructions on your phone.', 'success')
+        return redirect(url_for('page2', is_premium=True))
 
 
 ''' =====> Financial Analysis routes & logic <===== '''
 
 @app.route('/financial_analysis')
 def financial_analysis():
+    """
+    Render the Financial Analysis page.
+
+    If the user is not logged in, they are redirected to the login page.
+    This route serves the 'financial_analysis.html' template, which provides 
+    financial analysis tools and content for the user.
+
+    Returns:
+        str: HTML template for the Financial Analysis page.
+    """
     if 'user' not in session:
         return redirect(url_for('login'))
     return render_template('financial_analysis.html')
 
 @app.route('/page1')
 def page1():
+    """
+    Render the first financial analysis page showing July To Date Safaricom Share Prices.
+
+    If the user is not logged in, they are redirected to the login page.
+    This route reads CSV data for share prices, generates a Plotly line chart, 
+    and renders it in the 'page1.html' template.
+
+    Returns:
+        str: HTML template with an embedded Plotly graph showing share prices.
+    """
     if 'user' not in session:
         return redirect(url_for('login'))
 
@@ -423,6 +687,16 @@ def page1():
 
 @app.route('/page2')
 def page2():
+    """
+    Render the second financial analysis page showing June To Date Safaricom Share Prices.
+
+    If the user is not logged in, they are redirected to the login page.
+    This route reads CSV data for share prices, generates a Plotly line chart, 
+    and renders it in the 'page2.html' template.
+
+    Returns:
+        str: HTML template with an embedded Plotly graph showing share prices.
+    """
     if 'user' not in session:
         return redirect(url_for('login'))
 
